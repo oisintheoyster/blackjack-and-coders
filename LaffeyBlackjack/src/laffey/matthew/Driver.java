25d46159919d923dfc1541686b9b5563751945b0
Matthew <lafma@utschools.ca>
2017-05-21 12:28:14 -0400

    added the comment "//I'm so cool"

--- LaffeyBlackjack/src/laffey/matthew/Driver.java
+++ LaffeyBlackjack/src/laffey/matthew/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I wrote this code on my computer in eclipse, if this gets on github, im a friggin genius");
-
+		// im so cool
 	}
 
 }
