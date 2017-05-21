576b2e7a497ce0a8482728cbcfa35f1a7eb3b3c7
Matthew <lafma@utschools.ca>
2017-05-21 12:24:07 -0400

    Trying to see if I can upload code from eclipse directly to github without this copy paste shenanigans!

---
+++ LaffeyBlackjack/src/laffey/matthew/Driver.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package laffey.matthew;
+
+/**
+ * @author Matthew
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I wrote this code on my computer in eclipse, if this gets on github, im a friggin genius");
+
+	}
+
+}
